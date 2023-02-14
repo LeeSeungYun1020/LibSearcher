45c91a4bcc3f3d0e3a37d608b4963a4483289640
Lee Seung Yun <ileilliat@gmail.com>
2023-02-14 23:15:09 +0900

    Display initial data at HomeActivity

--- searcher/src/main/java/com/leeseungyun1020/searcher/HomeActivity.kt
+++ searcher/src/main/java/com/leeseungyun1020/searcher/HomeActivity.kt
@@ -2,10 +2,18 @@ package com.leeseungyun1020.searcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.leeseungyun1020.searcher.databinding.ActivityHomeBinding
+import com.leeseungyun1020.searcher.utilities.ExtraNames
+import com.leeseungyun1020.searcher.utilities.Type
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding.type = Type.valueOf(intent.getStringExtra(ExtraNames.Home.type) ?: Type.values().first().name)
+        binding.id = intent.getStringExtra(ExtraNames.Home.id) ?: ""
+        binding.pw = intent.getStringExtra(ExtraNames.Home.pw) ?: ""
+        setContentView(binding.root)
     }
 }
--- searcher/src/main/java/com/leeseungyun1020/searcher/utilities/start.kt
+++ searcher/src/main/java/com/leeseungyun1020/searcher/utilities/start.kt
@@ -7,7 +7,7 @@ import com.leeseungyun1020.searcher.HomeActivity
 fun Context.start(type: Type, clientId: String, clientPw: String) {
     startActivity(
         Intent(this, HomeActivity::class.java)
-            .putExtra(ExtraNames.Home.type, type)
+            .putExtra(ExtraNames.Home.type, type.name)
             .putExtra(ExtraNames.Home.id, clientId)
             .putExtra(ExtraNames.Home.pw, clientPw)
     )
