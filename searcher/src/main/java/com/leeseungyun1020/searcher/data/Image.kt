925a3d05fa7e541a8a324c822b814403a46d3e91
Lee Seung Yun <ileilliat@gmail.com>
2023-02-16 17:01:27 +0900

    Add item data class

---
+++ searcher/src/main/java/com/leeseungyun1020/searcher/data/Image.kt
@@ -0,0 +1,7 @@
+package com.leeseungyun1020.searcher.data
+
+data class Image(
+    val title: String,
+    val imageUrl: String,
+    val url: String,
+)
---
+++ searcher/src/main/java/com/leeseungyun1020/searcher/data/News.kt
@@ -0,0 +1,9 @@
+package com.leeseungyun1020.searcher.data
+
+data class News(
+    val title: String,
+    val date: String,
+    val imageUrl: String,
+    val contents: String,
+    val url: String,
+)
