3ae461d219739b6904c4db5ddea2748c191fed53
Lee Seung Yun <ileilliat@gmail.com>
2023-02-17 17:10:27 +0900

    Add QueryString class

---
+++ searcher/src/main/java/com/leeseungyun1020/searcher/network/QueryString.kt
@@ -0,0 +1,9 @@
+package com.leeseungyun1020.searcher.network
+
+class QueryString(val string: String) {
+    fun addQuery(key: String, value: String) = QueryString("$this&$key$value")
+
+    override fun toString() = string
+}
+
+fun String.addQuery(key: String, value: String) = QueryString("$this?$key=$value")
