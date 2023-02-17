6780b0d51ea4cea7edddf1facd41b0a915faf284
Lee Seung Yun <ileilliat@gmail.com>
2023-02-17 17:26:24 +0900

    Add image url check logic

--- searcher/src/main/java/com/leeseungyun1020/searcher/adapters/BindingAdapters.kt
+++ searcher/src/main/java/com/leeseungyun1020/searcher/adapters/BindingAdapters.kt
@@ -1,5 +1,6 @@
 package com.leeseungyun1020.searcher.adapters
 
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.nostra13.universalimageloader.core.ImageLoader
@@ -8,5 +9,7 @@ import com.nostra13.universalimageloader.core.ImageLoader
 fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
     if (!imageUrl.isNullOrEmpty()) {
         ImageLoader.getInstance().displayImage(imageUrl, view)
+    } else {
+        view.visibility = View.GONE
     }
 }
