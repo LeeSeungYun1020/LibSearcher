63818488e38b72ba0f2cdcfbb16e0c73e0a7ce72
Lee Seung Yun <ileilliat@gmail.com>
2023-02-20 16:37:48 +0900

    Modify news item date format

--- searcher/src/main/java/com/leeseungyun1020/searcher/network/KakaoNewsResponse.kt
+++ searcher/src/main/java/com/leeseungyun1020/searcher/network/KakaoNewsResponse.kt
@@ -2,6 +2,8 @@ package com.leeseungyun1020.searcher.network
 
 import com.google.gson.annotations.SerializedName
 import com.leeseungyun1020.searcher.data.News
+import java.text.SimpleDateFormat
+import java.util.*
 
 data class KakaoNewsResponse(
     @SerializedName("documents") val items: List<KakaoNews>
@@ -13,11 +15,20 @@ data class KakaoNews(
     @SerializedName("url") val url: String,
     @SerializedName("datetime") val date: String
 ) {
-    fun toNews() = News(
-        title = title,
-        date = date,
-        imageUrl = "",
-        contents = contents,
-        url = url
-    )
+    fun toNews(): News {
+        val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.ENGLISH)
+        val targetFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        val formattedDate =
+            format.parse(date)
+                ?.let { targetFormat.format(it) }
+                ?: date
+
+        return News(
+            title = title,
+            date = formattedDate,
+            imageUrl = "",
+            contents = contents,
+            url = url
+        )
+    }
 }
--- searcher/src/main/java/com/leeseungyun1020/searcher/network/NaverNewsResponse.kt
+++ searcher/src/main/java/com/leeseungyun1020/searcher/network/NaverNewsResponse.kt
@@ -2,6 +2,8 @@ package com.leeseungyun1020.searcher.network
 
 import com.google.gson.annotations.SerializedName
 import com.leeseungyun1020.searcher.data.News
+import java.text.SimpleDateFormat
+import java.util.*
 
 data class NaverNewsResponse(
     @SerializedName("items") val items: List<NaverNews>
@@ -13,11 +15,21 @@ data class NaverNews(
     @SerializedName("description") val contents: String,
     @SerializedName("pubDate") val date: String
 ) {
-    fun toNews(): News = News(
-        title = title,
-        date = date,
-        imageUrl = "",
-        contents = contents,
-        url = url
-    )
+    fun toNews(): News {
+        val format = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH)
+        val targetFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        val formattedDate =
+            format.parse(date)
+                ?.let { targetFormat.format(it) }
+                ?: date
+
+
+        return News(
+            title = title,
+            date = formattedDate,
+            imageUrl = "",
+            contents = contents,
+            url = url
+        )
+    }
 }
